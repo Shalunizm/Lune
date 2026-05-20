63246f726dedee436deb73870d974f8005367d43
MrDemonc <mrdemonlich@gmail.com>
2026-05-20 18:04:03 -0500

    Update fastlane for Lune 1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.demonlab.lune"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.1.0"
+        versionCode = 4
+        versionName = "1.1.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
